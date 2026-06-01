70d697fcf0e6bcb40c734111f13c11dfaf80e163
Hitesh-Wadhwani <hiteshwadwani555@gmail.com>
2026-06-01 23:09:55 +0530

    React Router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/footer'
+import Home from './components/Home/Home'
+
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
